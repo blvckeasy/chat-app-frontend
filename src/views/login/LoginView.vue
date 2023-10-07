29badf7bc167f37215eeadefe1927e50406000d6
Abdurahmonov Islom <islomabdurakhmonov8@gmail.com>
2023-10-07 14:28:36 +0500

    login and register views wroted

--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -1,5 +1,22 @@
 <template>
-    <div>Login</div>
+    <div class="container">
+        <h3 class="login-title">Login</h3>
+
+        <span class="email-title">Email</span>
+        <input type="text" placeholder="enter your email" class="email-input">
+
+        <span class="password-title">Password</span>
+        <input type="password" placeholder="enter your password" class="password-input">
+
+        <div class="remember-me-box">
+            <input type="checkbox" class="remember-me">
+            <label for="remember-me" class="remember-me-label">Remember me</label>
+        </div>
+
+        <a href="/register" class="signup-link">Create account?</a>
+
+        <button class="send-button">Send</button>
+    </div>
 </template>
 
 <script src="./login.js"></script>
--- src/views/register/RegisterView.vue
+++ src/views/register/RegisterView.vue
@@ -1,5 +1,25 @@
 <template>
-    <div>Register</div>
+    <div class="container">
+        <h3 class="register-title">Register</h3>
+
+        <span class="email-title">Email</span>
+        <input type="text" placeholder="enter your email" class="email-input">
+
+        <span class="password-title">Password</span>
+        <input type="password" placeholder="enter your password" class="password-input">
+
+        <span class="confirm-password-title">Confirm Password</span>
+        <input type="password" placeholder="retry password" class="confirm-password-input">
+
+        <div class="remember-me-box">
+            <input type="checkbox" class="remember-me">
+            <label for="remember-me" class="remember-me-label">Remember me</label>
+        </div>
+
+        <a href="/login" class="signin-link">Do you have already account?</a>
+
+        <button class="send-button">Send</button>
+    </div>
 </template>
 
 <script src="./register.js"></script>
